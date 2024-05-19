9cde6ede467b90fb8db303c6b5289d02f478d4a2
NguyenBich11 <ntngocbich11122004@gmail.com>
2024-05-19 23:39:42 +0700

    refacter code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -3,26 +3,27 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const morgan = require('morgan');
+const { log } = require('console');
 const engine = require('express-handlebars').engine;
+
+const route = require('./routes');
 
 // Define path img
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
